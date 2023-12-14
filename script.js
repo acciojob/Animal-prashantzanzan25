97b99e4feb6289806494a983cd390cdcd3e95cd1
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-12-14 12:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+// cypress/integration/tests/test.spec.js
+
+describe("Basic Rendering and Components", () => {
+  beforeEach(() => {
+    cy.visit("your-website-url"); // Replace with the URL of your website
+  });
+
+  it("Checking the heading", () => {
+    cy.get("#photo-heading")
+      .invoke("text")
+      .then((text) => {
+        console.log("Actual Text:", text);
+      });
+
+    cy.get("#photo-heading").should("have.text", "Animal Photos".trim());
+  });
+
+  it("Checking the Nesting inside", () => {
+    cy.get("#animal-photos").find("img").should("exist").and("have.length", 9);
+  });
+
+  // Add more test cases as needed
+});
